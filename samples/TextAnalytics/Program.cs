c9f4b4a391d123b58038f82620e6531b20d68af1
Aaron Hill <aahi@microsoft.com>
2019-02-07 09:38:36 -0800

    updating sample

--- samples/TextAnalytics/Program.cs
+++ samples/TextAnalytics/Program.cs
@@ -1,127 +1,165 @@
+
 using System;
 using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
 using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
 using System.Collections.Generic;
+using Microsoft.Rest;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Microsoft.Azure.CognitiveServices.Samples.TextAnalytics
+namespace TextAnalytics
 {
-    public static class Program
+    class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Container for subscription credentials. Replace this with your valid Text Analytics subscription key.
+        private const string SubscriptionKey = "enter-your-key-here";
+
+        /// </summary>
+        class ApiKeyServiceClientCredentials : ServiceClientCredentials
         {
-            RunSampleAsync("ENTER YOUR ENDPOINT HERE", "ENTER YOUR KEY HERE").Wait();
-            Console.WriteLine("Press ENTER to exit.");
-            Console.ReadLine();
+            public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                request.Headers.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+                return base.ProcessHttpRequestAsync(request, cancellationToken);
+            }
         }
 
-        public static async Task RunSampleAsync(string endpoint, string key)
+
+        /// <summary>
+        /// Uses the Text Analytics client to perform sentiment analysis on sample documents.
+        /// </summary>
+        static void sentimentAnalysis(ITextAnalyticsClient client)
         {
-            // Create a client.
-            ITextAnalyticsClient client = new TextAnalyticsClient(new ApiKeyServiceClientCredentials(key))
-            {
-                Endpoint = endpoint
-            };
-
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-            // Extracting language
-            Console.WriteLine("===== LANGUAGE EXTRACTION ======");
-
-            LanguageBatchResult result = await client.DetectLanguageAsync(
-                    new BatchInput(
-                        new List<Input>()
-                        {
-                          new Input("1", "This is a document written in English."),
-                          new Input("2", "Este es un document escrito en Español."),
-                          new Input("3", "这是一个用中文写的文件")
-                        }));
-
-            // Printing language results.
-            foreach (var document in result.Documents)
-            {
-                Console.WriteLine("Document ID: {0} , Language: {1}", document.Id, document.DetectedLanguages[0].Name);
-            }
-
-            // Getting key-phrases
-            Console.WriteLine("\n\n===== KEY-PHRASE EXTRACTION ======");
-
-            KeyPhraseBatchResult result2 = await client.KeyPhrasesAsync(
-                    new MultiLanguageBatchInput(
-                        new List<MultiLanguageInput>()
-                        {
-                          new MultiLanguageInput("ja", "1", "猫は幸せ"),
-                          new MultiLanguageInput("de", "2", "Fahrt nach Stuttgart und dann zum Hotel zu Fu."),
-                          new MultiLanguageInput("en", "3", "My cat is stiff as a rock."),
-                          new MultiLanguageInput("es", "4", "A mi me encanta el fútbol!")
-                        }));
-
-
-            // Printing keyphrases
-            foreach (var document in result2.Documents)
-            {
-                Console.WriteLine("Document ID: {0} ", document.Id);
-
-                Console.WriteLine("\t Key phrases:");
-
-                foreach (string keyphrase in document.KeyPhrases)
-                {
-                    Console.WriteLine("\t\t" + keyphrase);
-                }
-            }
-
-            // Extracting sentiment
             Console.WriteLine("\n\n===== SENTIMENT ANALYSIS ======");
-
-            SentimentBatchResult result3 = await client.SentimentAsync(
-                    new MultiLanguageBatchInput(
+            
+            //The documents to be submitted for language detection. The ID can be any value.
+            var inputDocuments = new MultiLanguageBatchInput(
                         new List<MultiLanguageInput>()
                         {
                           new MultiLanguageInput("en", "0", "I had the best day of my life."),
                           new MultiLanguageInput("en", "1", "This was a waste of my time. The speaker put me to sleep."),
                           new MultiLanguageInput("es", "2", "No tengo dinero ni nada que dar..."),
                           new MultiLanguageInput("it", "3", "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura."),
-                        }));
+                        });
+
+            SentimentBatchResult result = client.SentimentAsync(inputDocuments).Result;
 
 
-            // Printing sentiment results
-            foreach (var document in result3.Documents)
+            // Print the sentiment results
+            foreach (var document in result.Documents)
             {
-                Console.WriteLine("Document ID: {0} , Sentiment Score: {1:0.00}", document.Id, document.Score);
+                Console.WriteLine($"Document ID: {document.Id} , Sentiment Score: {document.Score:0.00}");
+            }
+        } 
+
+        /// <summary>
+        /// Uses the Text Analytics client to detect the language these sample documents are written in.
+        /// </summary>
+        static void detectLanguage(ITextAnalyticsClient client)
+        {
+            Console.WriteLine("===== LANGUAGE DETECTION ======");
+
+            //The documents to be submitted for language detection. The ID can be any value.
+            var inputDocuments = new BatchInput(
+                    new List<Input>()
+                        {
+                          new Input("1", "This is a document written in English."),
+                          new Input("2", "Este es un document escrito en Español."),
+                          new Input("3", "这是一个用中文写的文件")
+                    });
+
+            var result = client.DetectLanguageAsync(inputDocuments).Result;
+
+            // Print the language results.
+            foreach (var document in result.Documents)
+            {
+                Console.WriteLine($"Document ID: {document.Id} , Language: {document.DetectedLanguages[0].Name}");
             }
 
-            // Extracting entities
-            Console.WriteLine("\n\n===== Entity Extraction ======");
+        }
 
-            EntitiesBatchResultV2dot1 result4 = await client.EntitiesAsync(
-                    new MultiLanguageBatchInput(
+        /// <summary>
+        /// Uses the Text Analytics client to extract the key phrases from sample documents.
+        /// </summary>
+        public static void keyPhraseExtraction(ITextAnalyticsClient client)
+        {
+            Console.WriteLine("\n\n===== KEY-PHRASE EXTRACTION ======");
+
+            //The documents to be submitted for key-phrase extraction. The ID can be any value.
+            var inputDocuments = new MultiLanguageBatchInput(
                         new List<MultiLanguageInput>()
                         {
-                          new MultiLanguageInput("en", "0", "Microsoft released win10. Microsoft also released Hololens"),
-                          new MultiLanguageInput("en", "1", "Microsoft is an IT company."),
-                          new MultiLanguageInput("es", "2", "Microsoft lanzó win10. Microsoft también lanzó Hololens"),
-                          new MultiLanguageInput("es", "3", "Microsoft es una empresa de TI."),
-                        }));
+                          new MultiLanguageInput("ja", "1", "猫は幸せ"),
+                          new MultiLanguageInput("de", "2", "Fahrt nach Stuttgart und dann zum Hotel zu Fu."),
+                          new MultiLanguageInput("en", "3", "My cat might need to see a veterinarian."),
+                          new MultiLanguageInput("es", "4", "A mi me encanta el fútbol!")
+                        });
 
+            KeyPhraseBatchResult result2 = client.KeyPhrasesAsync(inputDocuments).Result;
 
-            // Printing entity extraction results
-            foreach (var document in result4.Documents)
+            // Print the key phrases that were extracted
+            foreach (var document in result2.Documents)
             {
-                Console.WriteLine("Document ID: {0} ", document.Id);
+                Console.WriteLine($"Document ID: {document.Id} ");
+                Console.WriteLine("\t Key phrases:");
 
-                Console.WriteLine("\t Entities:");
+                foreach (string keyphrase in document.KeyPhrases)
+                {
+                    Console.WriteLine($"\t\t{keyphrase}");
+                }
+            }
+
+        }
+
+        static void detectEntities(ITextAnalyticsClient client) { 
+        
+            Console.WriteLine("\n\n===== ENTITY DETECTION ======");
+
+            //The documents to be submitted for entity detection.
+            var inputDocuments = new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>()
+                        {
+                          new MultiLanguageInput("en", "0", "The Great Depression began in 1929. By 1933, the GDP in The United States fell by 25%.")
+                        });
+
+            EntitiesBatchResultV2dot1 result = client.EntitiesAsync(inputDocuments).Result;
+
+            // Print the found entities
+            foreach (var document in result.Documents)
+            {
+                Console.WriteLine($"Document ID: {document.Id} ");
+
 
                 foreach (EntityRecordV2dot1 entity in document.Entities)
                 {
-                    Console.WriteLine("\t\tEntity Name: {0}", entity.Name);
-                    Console.WriteLine("\t\tWikipedia Language: {0}", entity.WikipediaLanguage);
-                    Console.WriteLine("\t\tWikipedia Url: {0}", entity.WikipediaUrl);
-                    Console.WriteLine("\t\tNumber of times appeared on the text: {0}", entity.Matches.Count);
-                    Console.WriteLine("\t\tEntity Type: {0}", entity.Type);
-                    Console.WriteLine("\t\tEntity SubType: {0}", entity.SubType);
-                    Console.WriteLine("\n");
+
+                    Console.WriteLine($" Entity name: {entity.Name} \n Wikipedia URL: {entity.WikipediaUrl}");
+                    Console.WriteLine($" Entity type: { entity.Type} \n Entity Sub-type: {entity.SubType}");
+                    Console.WriteLine("---------------------");
                 }
             }
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Create a Text Analytics client.
+            ITextAnalyticsClient client = new TextAnalyticsClient(new ApiKeyServiceClientCredentials())
+            {
+                //Replace 'westus' with the correct region for your Text Analytics subscription
+                Endpoint = "https://westus.api.cognitive.microsoft.com"
+            }; 
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            detectLanguage(client);
+            keyPhraseExtraction(client);
+            sentimentAnalysis(client);
+            detectEntities(client);
+
+            Console.ReadLine();
         }
     }
 }
